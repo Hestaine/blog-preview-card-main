01c8de9658fd23e83ccd09ca6afcb0a5a8082f32
Hestaine <ariffhestaine@gmail.com>
2024-01-15 21:46:56 +0100

    jjjw

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,12 @@ module.exports = {
   content: ["./dist/*.{html,js}"],
   theme: {
     extend: {
+      colors: {
+        Yellow: "hsl(47, 88%, 63%)",
+        White: "hsl(0, 0%, 100%)",
+        Grey: "hsl(0, 0%, 50%)",
+        Black: "hsl(0, 0%, 7%)"
+      },
       fontFamily: {
         sans: ['Figtree', 'sans-serif'],
       }}
